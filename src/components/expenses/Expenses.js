0d3b82b9c9fa73b46781c8f94281c51aea0b0f3d
epanlauw <stefanus.lauwrendo@codify.id>
2021-07-21 20:53:01 +0700

    Chart Bar

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -4,6 +4,7 @@ import ExpensesList from "./ExpensesList";
 import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 import "./Expenses.css";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -22,6 +23,7 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
     </Card>
   );
